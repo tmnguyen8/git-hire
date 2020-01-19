4fbdaeb721007eb2a7962dcde3cdabf5a00ed401
Romit Chaudhari <romitchaudhari@romits-mbp.attlocal.net>
2020-01-19 13:28:14 -0500

    foundation for monster API

--- routes/api/MonsterRoute.js
+++ routes/api/MonsterRoute.js
@@ -7,20 +7,30 @@ router.get("/monster", (req, res) => {
   axios
     .get(`https://www.monster.com/jobs/search/?q=${req.query.title}&where=${req.query.location}`)
     .then((response) => {
+      // console.log(response);
       var $ = cheerio.load(response.data);
+      console.log($)
+      
       var scrapeData = [];
 
-      $("#SearchResults").each(()=>{
+      $("SearchResults").each(function(){
         var result = {}
         
-        // result.id = $(this);
-        // result.title = $(this);
-        // result.company = $(this);
-        // result.url = $(this);
-        // result.location = $(this);
-        // result.description = $(this);
-        // result.salary = $(this);
-        // result.imageURL = $(this);
+        result.id = $(this);
+        result.title = $(this);
+        result.company = $(this);
+        result.url = $(this);
+        result.location = $(this);
+        result.description = $(this);
+      
+        console.log("This is the id" + result.id)
+        console.log("This is the title" + result.title)
+        console.log("This is the company" + result.company)
+        console.log("This is the link" + result.url)
+        console.log("This is the location" + result.location)
+        console.log("This is the summary" + result.description)
+        console.log("===========================================================================")
+
 
         scrapeData.push(result)
       })
