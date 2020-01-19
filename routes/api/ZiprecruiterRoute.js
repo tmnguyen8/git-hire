442d8035ac16304eda96877c79d0fbb6008fc572
patrick Yeung <patrick.yeung7@gmail.com>
2020-01-18 22:31:53 -0500

    Unable to find the error on ziprecuriterRoute.js for web scraping

--- routes/api/ZiprecruiterRoute.js
+++ routes/api/ZiprecruiterRoute.js
@@ -1,20 +1,36 @@
 const axios = require("axios");
+// const request = require('request')
 const router = require("express").Router();
 const cheerio = require('cheerio');
 
+// request('https://www.ziprecruiter.com/candidate/search?search=${req.query.title}&location=${req.query.location}', (error, response, html) => {
+//   if (!error && response.statusCode == 200) {
+//     const $ = cheerio.load(html);
+    
+//     $('.job_content').each((i, el) => {
+//       const title = $(el)
+//       .find('.job_title')
+//       .text();
+//       console.log(title)
+//     })
+//   }
+// });
 router.get("/ziprecruiter", (req, res) => {
   // console.log(req.query)
   axios
     .get(`https://www.ziprecruiter.com/candidate/search?search=${req.query.title}&location=${req.query.location}`)
     .then((response) => {
-      var $ = cheerio.load(response.data);
+      const $ = cheerio.load(response.data);
       var scrapeData = [];
 
-      $("li").each(()=>{
+      $(".job_content").each(()=>{
         var result = {}
         
         // result.id = $(this);
-        // result.title = $(this);
+        result.title = $(this)
+        .find('h2')
+        .text();
+        console.log(result.title)
         // result.company = $(this);
         // result.url = $(this);
         // result.location = $(this);
