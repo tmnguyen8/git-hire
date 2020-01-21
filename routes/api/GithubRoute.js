84a3acadca79535f2f5fe813f2d2192206ae6fb2
Trai Nguyen <tnguyen@at-pac.com>
2020-01-21 10:37:51 -0500

    modify github api

--- routes/api/GithubRoute.js
+++ routes/api/GithubRoute.js
@@ -5,9 +5,28 @@ router.get("/github", (req, res) => {
   // console.log(req.query)
   axios
     .get(`https://jobs.github.com/positions.json?description=${req.query.title}&location=${req.query.location}`)
-    .then((results) => {
-      console.log(results.data)
-      res.json(results.data)
+    .then((response) => {
+      // console.log(response.data)
+
+      var responseData = [];
+
+      response.data.forEach(element => {
+        var result = {}
+        result.id = element.id;
+        result.title = element.title;
+        result.company = element.company;
+        result.url = element.url;
+        result.location = element.location;
+        result.description = element.description;
+        result.salary
+        result.company_logo = element.company_logo;
+        result.created_at = element.created_at; 
+
+        responseData.push(result)
+      });
+      console.log(responseData)
+
+      res.json(responseData)
     })
     .catch(err => res.status(422).json(err));
 });
