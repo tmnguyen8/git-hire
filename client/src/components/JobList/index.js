14b6b95b767afd5cf56ddac64d6b3e5829a1173b
Romit Chaudhari <romitchaudhari@romits-mbp.attlocal.net>
2020-01-20 22:00:32 -0500

    added location and salary functionality

--- client/src/components/JobList/index.js
+++ client/src/components/JobList/index.js
@@ -2,18 +2,20 @@ import React from "react";
 import Thumbnail from "../Thumbnail";
 import { Container, Row, Col } from "../Grid";
 
-// Exporting both RecipeList and RecipeListItem from this file
+// Exporting both JobList and JobListItem from this file
 
-// RecipeList renders a bootstrap list item
-export function RecipeList({ children }) {
+// JobList renders a bootstrap list item
+export function JobList({ children }) {
   return <ul className="list-group">{children}</ul>;
 }
 
-// RecipeListItem renders a bootstrap list item containing data from the recipe api call
-export function RecipeListItem({
+// JobListItem renders a bootstrap list item containing data from the recipe api call
+export function JobListItem({
   thumbnail = "https://placehold.it/300x300",
   title,
+  location, 
   description,
+  salary,
   href
 }) {
   return (
@@ -25,7 +27,9 @@ export function RecipeListItem({
           </Col>
           <Col size="xs-8 sm-9">
             <h3>{title}</h3>
+            <div>Location: {location}</div>
             <div>Description: {description}</div>
+            <div>Salary: {salary}</div>
             <a rel="noreferrer noopener" target="_blank" href={href}>
               Go to job!
             </a>
