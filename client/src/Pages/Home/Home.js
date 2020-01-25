cfef0164ec75ff296be265c654b347744119cc09
patrick Yeung <patrick.yeung7@gmail.com>
2020-01-25 03:49:43 -0500

    unable to link background image on front page

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Grid from '@material-ui/core/Grid'
+
+
+class Home extends Component {
+    state = {}
+  
+    render(){
+        return(
+            <div>
+
+                <Grid item xs={12}  container direction="row" justify="space-around"
+  alignItems="stretch">
+     </Grid>
+            </div>
+        )
+    }
+}
+
+export default Home
