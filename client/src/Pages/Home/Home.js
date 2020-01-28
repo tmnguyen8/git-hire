64925e3f4c1ed88f6398bd6816df7ab46924d4d6
patrick Yeung <patrick.yeung7@gmail.com>
2020-01-27 23:58:59 -0500

    restructring app.js file for app component, home page still need works and connect the link. DO NOT MERGE

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,17 +1,49 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './home.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Grid from '@material-ui/core/Grid'
-
+import Col from "../../components/Grid"
 
 class Home extends Component {
     state = {}
-  
+   
     render(){
         return(
             <div>
+                <header>
+                    <ButtonGroup variant="variant" color="primary">
+                        <Button>Sign up</Button>
+                        <Button>Login</Button>
+                    </ButtonGroup>
+                </header>
 
-                <Grid item xs={12}  container direction="row" justify="space-around"
-  alignItems="stretch">
-     </Grid>
+                <body>
+
+                    
+
+                    <div className="mainBG">
+                        <Grid item xs = { 2 }>
+                        <   Grid  item xs = {6 } className="mainContent">
+                            
+                                <p>Connect to your next<span className="text-holder"> </span></p>
+                           
+                            </Grid>
+                        </Grid>
+                    </div>
+                   
+                </body>
+
+                    {/* <div className="mainBG">
+                        <div className="mainContent">
+                            <p>Your next developer</p>
+                        </div>
+                    </div> */}
+
+                    <footer></footer>
+                
+       
             </div>
         )
     }
