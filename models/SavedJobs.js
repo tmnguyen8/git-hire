573e50e2ad0cb0c7402f1f84d989a2870dee88db
Trai Nguyen <tnguyen@at-pac.com>
2020-01-29 10:11:25 -0500

    adding structure for mongoDB

---
+++ models/SavedJobs.js
@@ -0,0 +1,18 @@
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+
+// const NoteSchema = new Schema({
+//     title: {
+//         type: String,
+//         required: false
+//     },
+//     body: {
+//         type: String,
+//         required: false
+//     }
+// })
+
+// var Note = mongoose.model('Note', NoteSchema);
+
+// module.exports = Note;
--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(cors());
 // Use apiRoutes
 app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userDB");
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
