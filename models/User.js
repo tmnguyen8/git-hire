45994839a5865b28f7d66205b8c344adf5e62822
Romit Chaudhari <romitchaudhari@Romits-MacBook-Pro.local>
2020-01-30 21:02:41 -0500

    updated models for db

--- models/User.js
+++ models/User.js
@@ -1,29 +1,33 @@
-// const mongoose = require('mongoose');
-// // Schema constructor
-// const Schema = mongoose.Schema;
-// const DealSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     link: {
-//         type: String,
-//         required: true
-//     },
-//     imageURL: {
-//         type: String,
-//         required: true
-//     },
-//     // note is an object that storage a NOTE id
+ const mongoose = require('mongoose');
+// Schema constructor
+ const Schema = mongoose.Schema;
+ const UserSchema = new Schema({
+     id: {
+         type: String,
+         required: "User ID is not present"
+     },
+     username: {
+         type: String,
+         required: "Username is not present"
+     },
+     profileURL: {
+         type: String,
+         required: "profile link is not present"
+     },
+     photos: {
+         type: String
+     },
+     // note is an object that storage a NOTE id
 //     // it reference the ObjectID of Note model
-//     note: {
-//         type: Schema.Types.ObjectId,
-//         ref: "Note"
-//     }
-// });
-// const Deal = mongoose.model('Deal', DealSchema);
+savedJob: {
+    type: UserSchema.Types.ObjectId,
+    ref: "Note"
+}
+});
 
-// module.exports = Deal;
+ const Deal = mongoose.model('User', UserSchema);
+
+ module.exports = savedJob;
 
 const mongoose = require('mongoose');
 
