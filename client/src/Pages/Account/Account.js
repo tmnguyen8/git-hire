95bf59fd2707f09c0593d6eb14c6103c697988e2
Patrick <patrick.yeung7@gmail.com>
2020-02-01 12:09:15 -0500

    succesfully linked the github user info to component

--- client/src/Pages/Account/Account.js
+++ client/src/Pages/Account/Account.js
@@ -2,19 +2,19 @@ import React from "react";
 import GlobalContext from "../../Context/globalContext";
 import Search from "../../components/Search";
 import Nav from "../../components/Nav";
-
+import Profile from "../../components/Profile"
 class Account extends React.Component {
   state = {};
   
   render() {
-    const global = this.context;
-    console.log(global);
+
     return (
       <div>
         <Nav/>
-        <h1>User Page: {global.user.displayName}</h1>
-        <p>{global.user.displayName}</p>
+        <h1>User Page: {localStorage.user.displayName}</h1>
+        <p>{localStorage.username}</p>
         <Search />
+        <Profile />
       </div>
       
     );
